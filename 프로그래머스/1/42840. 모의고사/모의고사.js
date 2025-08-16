0a4c171f6c7d2b64e4b43dddc82431e4147f660e
Sujin KIM <56298540+SJ0826@users.noreply.github.com>
2025-08-16 14:41:17 +0900

    [level 1] Title: 모의고사, Time: 39.10 ms, Memory: 38.4 MB -BaekjoonHub

---
+++ 프로그래머스/1/42840. 모의고사/모의고사.js
@@ -0,0 +1,30 @@
+function solution(answers) {
+    // 1. 수포자들의 패턴
+    const patterns = [
+        [1, 2, 3, 4, 5],
+        [2, 1, 2, 3, 2, 4, 2, 5],
+        [3, 3, 1, 1, 2, 2, 4, 4, 5, 5]
+    ];
+    
+    // 2. 수포자들의 점수를 저장할 배열
+    const scores = [0, 0, 0];
+    
+    // 3. 각 수포자의 패턴과 정답이 얼마나 일치하는지 확인
+    for(const [i, answer] of answers.entries() ) {
+        for(const [j, pattern] of patterns.entries()) {
+            if (answer === pattern[i % pattern.length]) scores[j]++;
+        }
+    }
+    
+    console.log('scores', scores);
+    // 4. 가장 높은 점수 저장
+    const maxScore = Math.max(...scores);
+    
+    // 5. 가장 높은 점수를 받은 수포자들의 번호를 찾아서 배열에 담음
+    const highestScore = [];
+    for(let i = 0; i < scores.length; i++) {
+        if(scores[i] === maxScore) highestScore.push(i + 1);
+    }
+    
+    return highestScore;
+}
