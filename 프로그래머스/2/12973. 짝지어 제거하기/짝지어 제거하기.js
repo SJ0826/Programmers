a9d1d5870556543beb37568583b80dda2f3f6c02
Sujin KIM <56298540+SJ0826@users.noreply.github.com>
2025-08-17 15:23:52 +0900

    [level 2] Title: 짝지어 제거하기, Time: 59.27 ms, Memory: 41.8 MB -BaekjoonHub

---
+++ 프로그래머스/2/12973. 짝지어 제거하기/짝지어 제거하기.js
@@ -0,0 +1,13 @@
+function solution(s)
+{
+    const stack = [];
+    
+    for(let i of s){
+        stack.push(i);
+        if(stack[stack.length-1] == stack[stack.length -2]){
+            stack.pop();
+            stack.pop();
+        }
+    }
+    return stack.length == 0 ? 1 : 0;
+}
