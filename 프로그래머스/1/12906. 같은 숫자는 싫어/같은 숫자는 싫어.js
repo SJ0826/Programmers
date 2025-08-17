7cfe94e6d43e5d4c3ef0cdcbf50ea9d8c97398d6
Sujin KIM <56298540+SJ0826@users.noreply.github.com>
2025-08-17 23:56:49 +0900

    Update 같은 숫자는 싫어.js

--- 프로그래머스/1/12906. 같은 숫자는 싫어/같은 숫자는 싫어.js
+++ 프로그래머스/1/12906. 같은 숫자는 싫어/같은 숫자는 싫어.js
@@ -1,3 +1,10 @@
+/**
+[📖 나의 풀이]
+- 큐 알고리즘 사용
+- 시간 복잡도: O(n)
+- 공부한 알고리즘을 사용해보고자한 풀이지만, 오히려 코드량이 늘고 풀이 시간을 많이 소요했음.
+**/
+
 class Queue {
     constructor(initialArray = []) {
         this.item = [...initialArray];
@@ -53,3 +60,45 @@ function solution(arr) {
     
     return answer;
 }
+
+/**
+[📖 BF]
+- 단순 순회
+- 시간 복잡도: O(n)
+**/
+
+// 최종 추천 코드 1
+function solution(arr) {
+    if (arr.length === 0) return [];
+    
+    const answer = [arr[0]];
+    
+    for (let i = 1; i < arr.length; i++) {
+        if (arr[i] !== arr[i-1]) {
+            answer.push(arr[i]);
+        }
+    }
+    
+    return answer;
+}
+
+// 최종 추천 코드 2
+function solution(arr) {
+    return arr.filter((val, index) => val != arr[index + 1]);
+}
+
+/**
+[✅ 정리]
+- ⭐️ 모든 문제풀이에 스택/큐 자료구조를 구현해서 풀어야하는건 아님.
+- 스택/큐 자료구조를 구현해서 사용할 경우
+    - 연산 횟수 > 10만번
+    - 메모리 제한 빡빡
+    - 시뮬레이션 문제 (프린터, 프로세스 스케줄링 등)
+    - 레벨별 BFS (트리 문제)
+
+- 그냥 배열 사용할 경우
+    - 단순 BFS/DFS
+    - 연산 횟수 < 1만번
+    - 일회성 처리
+    - 코드 간결함 우선
+**/
